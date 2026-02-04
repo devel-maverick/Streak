c765598b3e0c49e8c726bf44ff048018549650df
devel-maverick <codemaverick00@gmail.com>
2026-02-05 02:22:12 +0530

    Fix __dirname for ES module

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,10 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoutes from "./src/routes/auth.routes.js";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express()
 const port = process.env.PORT || 3000
 app.use(express.json())
